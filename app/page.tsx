7be3e7d0c257c50f272514935874e44a08731ced
Barra Victor <victor.barra@student.howest.be>
2024-02-26 21:28:45 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          In-browser markdown editor <br className="hidden sm:inline" />
+          built with Nextjs, Chadcn/ui & Tailwind CSS.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
           Accessible and customizable components that you can copy and paste
